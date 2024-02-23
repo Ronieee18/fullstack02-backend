8b7cecc79d46bde88b3dc13687b1aca199421a0a
Ronieee18 <ronitparwani78@gmail.com>
2024-02-23 19:31:15 +0530

    commited

--- src/app.js
+++ src/app.js
@@ -15,7 +15,6 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://superlative-licorice-9598b9.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
